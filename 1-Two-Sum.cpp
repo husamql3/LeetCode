5b702e1d7c0dadff11ff250087e6e4677fa3aed8
Hüsam <husamahmud@gmail.com>
2026-07-01 16:23:50 +0300

    [1] [Time Beats: 37.66%] [Memory Beats: 5.19%] - LeetPush

--- 1-Two-Sum.cpp
+++ 1-Two-Sum.cpp
@@ -1,21 +1,21 @@
-1class Solution {
-2public:
-3    vector<int> twoSum(vector<int>& nums, int target) {
-4
-5        map<int, int> mp;
-6        int n = nums.size();
-7        for (int i = 0; i < n; i++) {
-8            mp[nums[i]] = i;
-9        }
-10
-11        for (int i = 0; i < n; i++) {
-12            int c = target - nums[i];
-13
-14            if (mp.count(c) && mp[c] != i) {
-15                return {i, mp[c]};
-16            }
-17        }
-18
-19        return {-1, -1};
-20    }
-21};
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+
+        map<int, int> mp;
+        int n = nums.size();
+        for (int i = 0; i < n; i++) {
+            mp[nums[i]] = i;
+        }
+
+        for (int i = 0; i < n; i++) {
+            int c = target - nums[i];
+
+            if (mp.count(c) && mp[c] != i) {
+                return {i, mp[c]};
+            }
+        }
+
+        return {-1, -1};
+    }
+};
